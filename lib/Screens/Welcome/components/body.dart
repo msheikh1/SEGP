a20ea5a1599787b633cb577d567e96b5fa25fd00
msheikh1 <masudrocks24@gmail.com>
2024-04-04 02:12:31 +0800

    Finished Teacher Login Design

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -8,49 +8,83 @@ import 'package:flutter_school/constants.dart';
 import 'package:flutter_school/components/round_button.dart';
 
 class Body extends StatelessWidget {
-  const Body({super.key});
+  const Body({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final AuthService _auth = AuthService();
     Size size = MediaQuery.of(context).size;
     return Background(
+
+      child: Center(
         child: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Preschool App",
-            style: TextStyle(fontWeight: FontWeight.bold),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center, // Align column at the start
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.1), // Adjust this value according to your needs
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Preschool App",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: size.height * 0.05),
+              RoundButton(
+                text: "Teacher",
+                press: () async {
+                  print("Building TeacherScreen");
+                  Navigator.pushNamed(context, '/login');
+                },
+                color: myDarkBlue,
+              ),
+              RoundButton(
+                text: "Parent",
+                press: () async {
+                  dynamic result = await _auth.signInAnon();
+                  if (result == null) {
+                    print("error sign in");
+                  } else {
+                    print("success!");
+                    print(result);
+                  }
+                },
+                color: myDarkBlue,
+              ),
+            ],
           ),
-          SizedBox(height: size.height * 0.05),
+        ),
+      ),
+    );
+  }
+}
+
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        children: [
           Image.asset(
-            "assets/images/schoolv3.png",
-            width: size.width * 0.9,
+            "assets/images/belize_flag.png",
+            width: size.width,
+            height: size.height,
+            fit: BoxFit.cover,
           ),
-          SizedBox(height: size.height * 0.05),
-          RoundButton(
-            text: "Teacher",
-            press: () async {
-              print("Building TeacherScreen");
-              Navigator.pushNamed(context, '/login');
-            },
-          ),
-          RoundButton(
-            text: "Parent",
-            press: () async {
-              dynamic result = await _auth.signInAnon();
-              if (result == null) {
-                print("error sign in");
-              } else {
-                print("success!");
-                print(result);
-              }
-            },
-            color: myPrimaryLightColor,
-          ),
+          child,
         ],
       ),
-    ));
+    );
   }
 }
